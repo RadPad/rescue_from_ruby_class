8d3bc3519f89337856054aa636ca28fa10bbb31e
Sunil Pinnamaneni <sunilpinnamaneni@gmail.com>
2015-12-06 20:34:25 -0800

    Initial commit created using 'bundle gem rescue_from_ruby_class'.

---
+++ lib/rescue_from_ruby_class.rb
@@ -0,0 +1,5 @@
+require "rescue_from_ruby_class/version"
+
+module RescueFromRubyClass
+  # Your code goes here...
+end
---
+++ spec/rescue_from_ruby_class_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe RescueFromRubyClass do
+  it 'has a version number' do
+    expect(RescueFromRubyClass::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
